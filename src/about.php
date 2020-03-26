94cecffb110e2f0df4ba634be0ae62d76ccf3a19
Daniel Collins <danielmcollins1996@gmail.com>
2020-03-25 20:32:37 -0700

    Reformatted about page

--- src/about.php
+++ src/about.php
@@ -5,16 +5,6 @@
 -->
 <?php include('header.php'); ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
-</head>
-<body>
     <h1 class="pageTitle">About Our Site</h1>
     <!--
         We can add more things if we feel like it, personal pictures etc.
