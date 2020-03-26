77496af0c3d420ae0d89185d13a6821880a382db
Daniel Collins <danielmcollins1996@gmail.com>
2020-03-25 21:00:07 -0700

    browse-movies basic markup

--- src/browse-movies.php
+++ src/browse-movies.php
@@ -1,2 +1,72 @@
 <?php include('header.php'); ?>
+<style>
+<?php include('browse-movies.css'); ?>
+</style>
+
+<section id="search">
+			<div id="filters_box">
+				<form id="filters" title="Movie Filters">
+					<h4>Movie Filters</h3>
+					<fieldset>
+						<legend><h5>Title</h5></legend>	
+						<input type="text" name="title"/>
+					</fieldset>
+					
+					<fieldset id="year_filters">
+						<legend><h5>Year</h5></legend>
+						<div>
+						<input type="radio" name="year_selector" value="before" id="before"/>
+						<label>Before</label>
+						<input type="number" id="before_year" min="1000"/>
+
+						<input type="radio" name="year_selector" value="after" id="after"/>
+						<label>After</label>
+						<input type="number" id="after_year" min="1000"/>
+
+						<input type="radio" name="year_selector" value="between" id="between"/>
+						<label>Between</label>
+						<input type="number" id="between_start" placeholder="1970" min="1000"/>
+						<input type="number" id="between_end" placeholder="1975" min="1000"/>
+						</div>
+					</fieldset>
+					<fieldset id="rating_filters">
+						<legend><h5>Rating</h5></legend>
+						<div>
+						<input type="radio" name="rating_selector" id="below"/>
+						<label>Below</label>
+						<input type="range" id="below_rating" min="0" max="10" value="10"/>
+						<p id="below_range_value" class="range_value">10</p>
+
+						<input type="radio" name="rating_selector" id="above"/>
+						<label>Above</label>
+						<input type="range" id="above_rating" min="0" max="10" value="0"/>
+						<p id="above_range_value" class="range_value">0</p>
+						
+						<input type="radio" name="rating_selector" id="between"/>
+						<label>Between</label>
+						<input type="range" id="between_start" min="0" max="10" value="0"/>
+						<p id="between_start_value" class="range_value">0</p>
+						<input type="range" id="between_end" min="0" max="10" value="10"/>
+						<p id="between_end_value" class="range_value">10</p>
+						</div>
+					</fieldset>
+					<div id="buttons">
+						<input type="button" id="update_filters" value="Filter"/>
+						<input type="reset" value="Clear"/>
+					</div>
+				</form>
+			</div>
+			<div id="matches">
+				<h4>List / Matches</h4>
+				<div id="matches-header">
+					<div id="title"><h5>Title</h5></div>
+					<h5>Year</h5>
+					<h5>Rating</h5>
+				</div>
+				<ul>
+					<li></li>
+
+				</ul>
+			</div>
+		</section>
 <?php include('footer.php'); ?>
--- src/header.php
+++ src/header.php
@@ -2,21 +2,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>Movies</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Movies</title>
+    <meta name="description" content="Site For Searching And Favoriting Movies">
+    <link rel="icon" href="./public/movie-symbol.ico">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
 <body>
     <nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
+    <div class="nav-wrapper">
+    <a href="index.php" class="left brand-logo">Home</a>
+    <ul class="right hide-on-small-and-down">
         <li><a href="about.php">About</a></li>
         <li><a href="browse-movies.php">Browse Movies</a></li>
         <li><a href="favorites.php">Favourites</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="sign-up.php">Sign-Up</a></li>
     </ul>
-    
+    </div>
     </nav>
 
 
