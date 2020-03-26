e519d3e6130751ce08dcec02e65415cd57398233
rpowe271 <35184251+rpowe271@users.noreply.github.com>
2020-03-25 21:03:52 -0600

    Added basic html
    We can add a real description at some point

--- src/about.php
+++ src/about.php
@@ -1,2 +1,45 @@
+<!--
+    Our "About" page, linked from the menu bar.
+    Only added basic text so far -- styling needed.
+    Personal github link for Max needed
+-->
 <?php include('header.php'); ?>
 <?php include('footer.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+</head>
+<body>
+    <h1 class="pageTitle">About Our Site</h1>
+    <!--
+        We can add more things if we feel like it, personal pictures etc.
+    -->
+    <p>COMP 3512-001 Winter 2020</p>
+    <p>Professor Randy Connolly</p>
+    <p>Mount Royal University</p>
+    <a href="https://github.com/eightys3v3n/comp3512_asg2">Access Our Github Here!</a>
+    <p id="desc">This is our movie database site....</p>
+
+    <p id="aboutName">Max Tiblenko</p>
+    <p>Email:<a href="mailto:mtibl397@mtroyal.ca">mtibl397@mtroyal.ca</a></p>
+    <a href="https://github.com/MAXL4RD">Personal Github</a>
+
+    <p id="aboutName">Terrence Plunkett</p>
+    <p>Email:<a href="mailto:tplun878@mtroyal.ca">tplun878@mtroyal.ca</a></p>
+    <a href="https://github.com/eightys3v3n/">Personal Github</a> 
+
+    <p id="aboutName">Daniel Collins</p> 
+    <p>Email:<a href="mailto:danielmcollins1996@gmail.com">danielmcollins1996@gmail.com</a></p>
+    <a href="https://github.com/DanielCollins96">Personal Github</a>
+
+    <p id="aboutName">Joseph Power Romero</p> 
+    <p>Email:<a href="mailto:rpowe271@mtroyal.ca">rpowe271@mtroyal.ca</a></p>
+    <a href="https://github.com/rpowe271">Personal Github</a>
+
+</body>
+</html>
