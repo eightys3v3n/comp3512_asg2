bd23c89877c01c15a549b57066d1a253eb3f60e5
EightyS3v3n <eightys3v3n@gmail.com>
2020-03-27 12:41:19 -0600

    Adding browse-movies code from ASG1; Changed submodule to HTTPS for MAXL4RD

---
+++ src/browse-movies.js
@@ -0,0 +1,307 @@
+const RANDY_API_URL = "http://www.randyconnolly.com/funwebdev/3rd/api/movie/";
+const API_MODE = "Randy";
+// const API_MODE = "Custom"; // for use with our API once built
+
+document.addEventListener("DOMContentLoaded", main);
+console.log("working");
+
+function Filter(title, year_between, rating_between) {
+	if (title) {
+		this.title = title.toLowerCase();
+	} else {
+		this.title = "";
+	}
+
+	if (year_between) {
+		console.assert(year_between.length === 2, "year_between must be an array with two elements");
+		if (year_between[0] && year_between[1]) {
+			console.assert(year_between[0] <= year_between[1], "After year must be <= than Before year when specified");
+		}
+		this.year = {after: year_between[0], before: year_between[1]};
+	}
+
+	if (rating_between) {
+		console.assert(rating_between.length === 2, "rating_between must be an array with two elements");
+		if (rating_between[0] && rating_between[1]) {
+			console.assert(rating_between[0] <= rating_between[1], "Above rating must be >= Below rating when specified");
+		}
+		this.rating = {above: rating_between[0], below: rating_between[1]};
+	}
+}
+
+
+function get_movies() {
+	try {
+		movies = JSON.parse(window.localStorage.getItem("movies"));
+		movies = movies.sort((a,b) => {
+			if (a.title < b.title) {
+				return -1;
+			} else if (a.title > b.title) {
+				return 1;
+			} else {
+				return 1;
+			}
+		});
+		populate_movies(movies);
+		hide_loading();
+	} catch(e) {
+		console.log("Failed to get movies from local storage.");
+	}
+
+	if (!movies) {
+		console.log("Downloading movies...");
+		show_loading();
+        let url;
+        
+        if (API_MODE == "Randy") {
+            url = RANDY_API_URL+"movies-brief.php?id=ALL";
+        } else if (API_MODE == "Custom") {
+            console.error("Custom API mode isn't implemented yet");
+        }
+        
+		fetch(url)
+			.then(response => response.json())
+			.then(data => {
+				window.localStorage.setItem("movies", JSON.stringify(data));
+				populate_movies(data)
+				hide_loading();
+			});
+	}
+}
+
+function show_loading() {
+	document.querySelector("#search #matches #loading").style.display = "";
+}
+
+function hide_loading() {
+	document.querySelector("#search #matches #loading").style.display = "none";
+}
+
+function main() {
+	get_movies();
+
+	document.querySelector("#filters")
+		.addEventListener("keyup", e => {
+			if (e.keyCode === 13) {
+				refresh_filters();
+			}
+		});
+	document.querySelector("#search #filters_box #hide_filters")
+		.addEventListener("click", toggle_filters);
+	document.querySelector("#search #filters #buttons #update_filters")
+		.addEventListener("click", refresh_filters);
+	document.querySelector("#search #filters #year_filters #before_year")
+		.addEventListener("click", e => {
+			document.querySelector("#search #filters #year_filters #before").checked = true;
+		});
+	document.querySelector("#search #filters #year_filters #after_year")
+		.addEventListener("click", e => {
+			document.querySelector("#search #filters #year_filters #after").checked = true;
+		});
+	document.querySelector("#search #filters #year_filters #between_start")
+		.addEventListener("click", e => {
+			document.querySelector("#search #filters #year_filters #between").checked = true;
+		});
+	document.querySelector("#search #filters #year_filters #between_end")
+		.addEventListener("click", e => {
+			document.querySelector("#search #filters #year_filters #between").checked = true;
+		});
+	document.querySelector("#search #filters #rating_filters #below_rating")
+		.addEventListener("click", e => {
+			document.querySelector("#search #filters #rating_filters #below").checked = true;
+		});
+	document.querySelector("#search #filters #rating_filters #above_rating")
+		.addEventListener("click", e => {
+			document.querySelector("#search #filters #rating_filters #above").checked = true;
+		});
+	document.querySelector("#search #filters #rating_filters #between_start")
+		.addEventListener("click", e => {
+			document.querySelector("#search #filters #rating_filters #between").checked = true;
+		});
+	document.querySelector("#search #filters #rating_filters #between_end")
+		.addEventListener("click", e => {
+			document.querySelector("#search #filters #rating_filters #between").checked = true;
+		});
+	document.querySelector("#search #filters #rating_filters #below_rating")
+		.addEventListener("click", e => {
+			document.querySelector("#search #filters #rating_filters #below_range_value").textContent = e.target.value;
+		});
+	document.querySelector("#search #filters #rating_filters #above_rating")
+		.addEventListener("click", e => {
+			document.querySelector("#search #filters #rating_filters #above_range_value").textContent = e.target.value;
+		});
+	document.querySelector("#search #filters #rating_filters #between_start")
+		.addEventListener("click", e => {
+			document.querySelector("#search #filters #rating_filters #between_start_value").textContent = e.target.value;
+		});
+	document.querySelector("#search #filters #rating_filters #between_end")
+		.addEventListener("click", e => {
+			document.querySelector("#search #filters #rating_filters #between_end_value").textContent = e.target.value;
+		});
+}
+
+function toggle_filters() {
+	let filters_box = document.querySelector("#search #filters");
+	let hide_btn = document.querySelector("#search #filters_box #hide_filters p");
+	let section = document.querySelector("#search");
+
+	if (filters_box.style.display == "none") {
+		filters_box.style.display = "";
+		hide_btn.textContent = "<";
+		section.style.gridTemplateColumns = "1fr 2fr";
+	} else {
+		filters_box.style.display = "none";
+		hide_btn.textContent = ">";
+		section.style.gridTemplateColumns = "2em auto";
+	}
+}
+
+function refresh_filters() {
+	let title = document.querySelector("#search #filters input[name='title']").value;
+	let year_between = get_year_between_filter();
+	let rating_between = get_rating_between_filter();
+
+	console.log(rating_between);
+	filter = new Filter(title, year_between, rating_between);
+	filtered_movies = filter_movies(filter);
+	populate_movies(filtered_movies);
+}
+
+function get_year_between_filter() {
+	let year = [undefined, undefined];
+	let before = document.querySelector("#search #filters #year_filters #before");
+	let after = document.querySelector("#search #filters #year_filters #after");
+	let between = document.querySelector("#search #filters #year_filters #between");
+
+	if (before.checked) {
+		year[1] = document.querySelector("#search #filters #year_filters #before_year").value;
+		year[1] = parseInt(year[1]);
+	} else if (after.checked) {
+		year[0] = document.querySelector("#search #filters #year_filters #after_year").value;
+		year[0] = parseInt(year[0]);
+	} else if (between.checked) {
+		year[0] = document.querySelector("#search #filters #year_filters #between_start").value;
+		year[1] = document.querySelector("#search #filters #year_filters #between_end").value;
+		year = [parseInt(year[0]), parseInt(year[1])];
+		year[1] += 1;
+	}
+
+	if (year[0] === "")
+		year[0] = undefined;
+	if (year[1] === "")
+		year[1] = undefined;
+
+	return year;
+}
+
+function get_rating_between_filter() {
+	let rating = [undefined, undefined];
+	let below = document.querySelector("#search #filters #rating_filters #below");
+	let above = document.querySelector("#search #filters #rating_filters #above");
+	let between = document.querySelector("#search #filters #rating_filters #between");
+
+	if (below.checked) {
+		rating[1] = document.querySelector("#search #filters #rating_filters #below_rating").value;
+		rating[1] = parseInt(rating[1]);
+	} else if (above.checked) {
+		rating[0] = document.querySelector("#search #filters #rating_filters #above_rating").value;
+		rating[0] = parseInt(rating[0]);
+	} else if (between.checked) {
+		rating[0] = document.querySelector("#search #filters #rating_filters #between_start").value;
+		rating[1] = document.querySelector("#search #filters #rating_filters #between_end").value;
+		rating = [parseInt(rating[0]), parseInt(rating[1])];
+	}
+
+	if (rating[0] === "") {
+		rating[0] = undefined;
+	}
+	if (rating[1] === "") {
+		rating[1] = undefined;
+	}
+
+	return rating;
+}
+
+function populate_movies(movies_list) {
+	let matches = document.querySelector("#search #matches ul");
+
+	matches.innerHTML = "";
+
+	if (movies_list.length === 0) {
+		document.querySelector("#search #matches #no_matches").style.display = "";
+	} else {
+		document.querySelector("#search #matches #no_matches").style.display = "none";
+		for (let movie of movies_list) {
+			add_movie(matches, movie);
+		}
+	}
+}
+
+function add_movie(element, movie) {
+	let li = document.createElement("li");
+	li.classList.add("match");
+
+	let img = document.createElement("img");
+	img.classList.add("icon");
+	img.src = poster_url + "w92" + movie.poster;
+	img.alt = "Movie Poster";
+	li.appendChild(img);
+
+	let h3 = document.createElement("h3");
+	h3.classList.add("title");
+	h3.textContent = movie.title;
+	li.appendChild(h3);
+
+	let year = document.createElement("p");
+	year.classList.add("year");
+	year.textContent = year_of(movie.release_date);
+	li.appendChild(year);
+
+	let rating = document.createElement("p");
+	rating.classList.add("rating");
+	rating.textContent = movie.ratings.average.toFixed(1);
+	li.appendChild(rating);
+
+	let view_btn = document.createElement("input");
+	view_btn.type = "button";
+	view_btn.name = "view";
+	view_btn.value = "View";
+	li.appendChild(view_btn);
+
+	element.appendChild(li);
+	li.addEventListener("click", e => {switch_page(pages.Details, movie)});
+}
+
+function year_of(date_str) {
+	return date_str.split("-")[0];
+}
+
+function filter_movies(filter) {
+	if (!movies) {
+		console.warn("Movies attempted to be populated but they haven't been fetched yet.");
+		return [];
+	}
+
+	let filtered_movies = [];
+	for (let movie of movies) {
+		if (!movie.title.toLowerCase().includes(filter.title)) // in production this would be a fuzzy search rather than includes.
+			continue;
+
+		let date = new Date(movie.release_date);
+		if (filter.year.before && filter.year.before <= date.getFullYear())
+			continue;
+		if (filter.year.after && filter.year.after > date.getFullYear())
+			continue;
+
+		let rating = movie.ratings.average;
+		if (filter.rating.below && rating > filter.rating.below)
+			continue;
+		if (filter.rating.above && rating < filter.rating.above)
+			continue;
+
+		filtered_movies.push(movie);
+	}
+
+	return filtered_movies;
+}
+
--- src/browse-movies.php
+++ src/browse-movies.php
@@ -1,72 +1,95 @@
 <?php include('header.php'); ?>
 <style>
-<?php include('browse-movies.css'); ?>
+  <?php include('browse-movies.css'); ?>
 </style>
-
+                  
 <section id="search">
-			<div id="filters_box">
-				<form id="filters" title="Movie Filters">
-					<h3>Movie Filters</h3>
-					<fieldset>
-						<legend><h4>Title</h4></legend>	
-						<input type="text" name="title"/>
-					</fieldset>
-					
-					<fieldset id="year_filters">
-						<legend><h4>Year</h4></legend>
-						<div>
-						<input type="radio" name="year_selector" value="before" id="before"/>
-						<label>Before</label>
-						<input type="number" id="before_year" min="1000"/>
+  <div id="filters_box">
+    <form id="filters" title="Movie Filters">
+      <h3>Movie Filters</h3>
+	  <fieldset>
+		<legend><h4>Title</h4></legend>	
+		<input type="text" name="title"/>
+	  </fieldset>
+	  
+	  <fieldset id="year_filters">
+		<legend><h4>Year</h4></legend>
+		<div>
+		  <input type="radio" name="year_selector" value="before" id="before"/>
+		  <label>Before</label>
+		  <input type="number" id="before_year" min="1000"/>
+          
+		  <input type="radio" name="year_selector" value="after" id="after"/>
+		  <label>After</label>
+		  <input type="number" id="after_year" min="1000"/>
+          
+		  <input type="radio" name="year_selector" value="between" id="between"/>
+		  <label>Between</label>
+		  <input type="number" id="between_start" placeholder="1970" min="1000"/>
+		  <input type="number" id="between_end" placeholder="1975" min="1000"/>
+        </div>
+	  </fieldset>
+	  <fieldset id="rating_filters">
+		<legend><h4>Rating</h4></legend>
+		<div>
+		  <input type="radio" name="rating_selector" id="below"/>
+		  <label>Below</label>
+		  <input type="range" id="below_rating" min="0" max="10" value="10"/>
+		  <p id="below_range_value" class="range_value">10</p>
+          
+		  <input type="radio" name="rating_selector" id="above"/>
+		  <label>Above</label>
+		  <input type="range" id="above_rating" min="0" max="10" value="0"/>
+		  <p id="above_range_value" class="range_value">0</p>
+		  
+		  <input type="radio" name="rating_selector" id="between"/>
+		  <label>Between</label>
+		  <input type="range" id="between_start" min="0" max="10" value="0"/>
+		  <p id="between_start_value" class="range_value">0</p>
+		  <input type="range" id="between_end" min="0" max="10" value="10"/>
+		  <p id="between_end_value" class="range_value">10</p>
+		</div>
+	  </fieldset>
+	  <div id="buttons">
+		<input type="button" id="update_filters" value="Filter"/>
+		<input type="reset" value="Clear"/>
+	  </div>
+	</form>
+    <div id="hide_filters">
+      <p>&lt</p>
+    </div>
+  </div>
+  <div id="matches">
+	<h3>List / Matches</h3>
+	<div id="matches-header">
+	  <div id="title"><h4>Title</h4></div>
+	  <h4>Year</h4>
+	  <h4>Rating</h4>
+	</div>
+	<div id="loading">
+	  <img src="https://media.giphy.com/media/WiIuC6fAOoXD2/giphy.gif" title="Loading">
+	  <p>Loading...</p>
+	</div>
+	<p id="no_matches">No matches found</p>
+	<ul>
+	  <li>
+		<img class="icon" src="https://assets.fireside.fm/file/fireside-images/podcasts/images/b/bc7f1faf-8aad-4135-bb12-83a8af679756/cover.jpg?v=3"></img>
+		<h3 class="title">This is a movie title</h3>
+		<p class="year">1999</p>
+		<p class="rating">1.1</p>
+		<input type="button" name="view" value="View"/>
+	  </li>
+	  <li>
+		<img class="icon" src="https://assets.fireside.fm/file/fireside-images/podcasts/images/b/bc7f1faf-8aad-4135-bb12-83a8af679756/cover.jpg?v=3"></img>
+		<h3 class="title">This is a movie title</h3>
+		<p class="year">1999</p>
+		<p class="rating">1.1</p>
+		<input type="button" name="view" value="View"/>
+	  </li>  
+	</ul>
+  </div>
+</section>
 
-						<input type="radio" name="year_selector" value="after" id="after"/>
-						<label>After</label>
-						<input type="number" id="after_year" min="1000"/>
+<script src="browse-movies.js"></script>
 
-						<input type="radio" name="year_selector" value="between" id="between"/>
-						<label>Between</label>
-						<input type="number" id="between_start" placeholder="1970" min="1000"/>
-						<input type="number" id="between_end" placeholder="1975" min="1000"/>
-						</div>
-					</fieldset>
-					<fieldset id="rating_filters">
-						<legend><h4>Rating</h4></legend>
-						<div>
-						<input type="radio" name="rating_selector" id="below"/>
-						<label>Below</label>
-						<input type="range" id="below_rating" min="0" max="10" value="10"/>
-						<p id="below_range_value" class="range_value">10</p>
-
-						<input type="radio" name="rating_selector" id="above"/>
-						<label>Above</label>
-						<input type="range" id="above_rating" min="0" max="10" value="0"/>
-						<p id="above_range_value" class="range_value">0</p>
-						
-						<input type="radio" name="rating_selector" id="between"/>
-						<label>Between</label>
-						<input type="range" id="between_start" min="0" max="10" value="0"/>
-						<p id="between_start_value" class="range_value">0</p>
-						<input type="range" id="between_end" min="0" max="10" value="10"/>
-						<p id="between_end_value" class="range_value">10</p>
-						</div>
-					</fieldset>
-					<div id="buttons">
-						<input type="button" id="update_filters" value="Filter"/>
-						<input type="reset" value="Clear"/>
-					</div>
-				</form>
-			</div>
-			<div id="matches">
-				<h3>List / Matches</h3>
-				<div id="matches-header">
-					<div id="title"><h4>Title</h4></div>
-					<h4>Year</h4>
-					<h4>Rating</h4>
-				</div>
-				<ul>
-					<li></li>
-
-				</ul>
-			</div>
-		</section>
 <?php include('footer.php'); ?>
