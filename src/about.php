b9b42f4e580462cb5a543336f74013e88c95c5c9
rpowe271 <35184251+rpowe271@users.noreply.github.com>
2020-03-28 18:19:53 -0600

    Formatting, styling

--- src/about.php
+++ src/about.php
@@ -1,7 +1,6 @@
 <!--
     Our "About" page, linked from the menu bar.
     Only added basic text so far -- styling needed.
-    Personal github link for Max needed
   -->
 <!DOCTYPE html>
 <html lang="en">
@@ -19,27 +18,39 @@
     <!--
         We can add more things if we feel like it, personal pictures etc.
     -->
-    <p>COMP 3512-001 Winter 2020</p>
-    <p>Professor Randy Connolly</p>
-    <p>Mount Royal University</p>
-    <a href="https://github.com/eightys3v3n/comp3512_asg2">Access Our Github Here!</a>
-    <p id="desc">This is our movie database site....</p>
+    <div id="aboutTop">
+      <p>COMP 3512-001 Winter 2020</p>
+      <p>Professor Randy Connolly</p>
+      <p>Mount Royal University</p>
+      <p><a href="https://github.com/eightys3v3n/comp3512_asg2" id="gitLink">Access Our Github Here!</a></p>
+      <p id="desc">This is our movie database site...</p>
+    </div>
 
-    <p id="aboutName">Max Tiblenko</p>
-    <p>Email:<a href="mailto:mtibl397@mtroyal.ca">mtibl397@mtroyal.ca</a></p>
-    <a href="https://github.com/MAXL4RD">Personal Github</a>
+    <ul class="aboutList">
+      <li>
+        <p id="aboutName">Max Tiblenko</p>
+        <p>Contact: <a href="mailto:mtibl397@mtroyal.ca">mtibl397@mtroyal.ca</a></p>
+        <a href="https://github.com/MAXL4RD">Link to Personal Github</a>
+      </li>
 
-    <p id="aboutName">Terrence Plunkett</p>
-    <p>Email:<a href="mailto:tplun878@mtroyal.ca">tplun878@mtroyal.ca</a></p>
-    <a href="https://github.com/eightys3v3n/">Personal Github</a> 
+      <li>
+        <p id="aboutName">Terrence Plunkett</p>
+        <p class="perLink">Contact: <a href="mailto:tplun878@mtroyal.ca">tplun878@mtroyal.ca</a></p>
+        <a href="https://github.com/eightys3v3n/">Link to Personal Github</a>
+      </li>
 
-    <p id="aboutName">Daniel Collins</p> 
-    <p>Email:<a href="mailto:danielmcollins1996@gmail.com">danielmcollins1996@gmail.com</a></p>
-    <a href="https://github.com/DanielCollins96">Personal Github</a>
+      <li>
+        <p id="aboutName">Daniel Collins</p> 
+        <p>Contact: <a href="mailto:danielmcollins1996@gmail.com">danielmcollins1996@gmail.com</a></p>
+        <a href="https://github.com/DanielCollins96">Link to Personal Github</a>
+      </li>
 
-    <p id="aboutName">Joseph Power Romero</p> 
-    <p>Email:<a href="mailto:rpowe271@mtroyal.ca">rpowe271@mtroyal.ca</a></p>
-    <a href="https://github.com/rpowe271">Personal Github</a>
+      <li>
+        <p id="aboutName">Joseph Power Romero</p> 
+        <p>Contact: <a href="mailto:rpowe271@mtroyal.ca">rpowe271@mtroyal.ca</a></p>
+        <a href="https://github.com/rpowe271">Link to Personal Github</a>
+      </li>
+    </ul>
 
     <?php include('footer.php'); ?>
   </body>
