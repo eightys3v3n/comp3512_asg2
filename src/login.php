faddb96724928fb4fab7a029d27ab8afb0dfded6
Daniel Collins <danielmcollins1996@gmail.com>
2020-03-31 19:05:33 -0700

    some login changes

--- src/login.php
+++ src/login.php
@@ -1,7 +1,14 @@
 <!--
     This page lets the user login with an email and password. What happens if they are already logged in?
   -->
-  <?php $page_title ='Login'; ?>
+  <?php $page_title ='Login'; 
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+    // username and password sent from form 
+  header("location: index.php");
+  }
+  
+  
+  ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -13,8 +20,14 @@
   <body>
     <?php include('header.php'); ?>
     <?php include('nav.php'); ?>
-
-    The login page! :D
+<div class = "box">
+    <form action = "" method = "post">
+      <label>Username  </label><input type = "text" name = "username" required /><br /><br />
+      <label>Password  </label><input type = "password" name = "password" required /><br/><br />
+      <input type = "submit" value = " Submit "/><br />
+    </form>
+</div>
+    <!-- The login page! :D -->
     
     <?php include('footer.php'); ?>
   </body>
