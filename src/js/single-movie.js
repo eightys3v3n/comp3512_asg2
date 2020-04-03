afc118baa7f24195f0c71f2cf7610720153966fd
MAXL4RD <51248905+MAXL4RD@users.noreply.github.com>
2020-04-03 16:02:56 -0600

    Cast and crew buttons now work and properly display

--- src/js/single-movie.js
+++ src/js/single-movie.js
@@ -8,29 +8,28 @@ function main()
     let crew = document.querySelector("#crew");
     let cast = document.querySelector("#cast");
     
-    crewBtn.addEventListener("click", changeTabs(crewBtn));
-    castBtn.addEventListener("click", changeTabs(castBtn));
+    crewBtn.addEventListener("click", changeTabs);
+    castBtn.addEventListener("click", changeTabs);
     
 }
 
 function changeTabs(e)
 {
-    let btn_val = e.value;
+    let btn_val = e.target.value;
         
     console.log(btn_val);
         
     if(btn_val == "Crew")
     {
-        crewBtn.classList.toggle("active");
-        castBtn.classList.toggle("hidden");
+        crewBtn.classList.add("active");
+        castBtn.classList.remove("active");
         crew.style.display = "grid";
         cast.style.display = "none";
     }
-    
-    if(btn_val == "Cast")
+    else if(btn_val == "Cast")
     {
-        crewBtn.classList.toggle("hidden");
-        castBtn.classList.toggle("active");
+        castBtn.classList.add("active");
+        crewBtn.classList.remove("active");
         crew.style.display = "none";
         cast.style.display = "grid";
     }
