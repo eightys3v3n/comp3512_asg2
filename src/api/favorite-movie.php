13bbbdeae53f2fef6c3af8badbbbecb14763cbfc
eightys3v3n <eightys3v3n@gmail.com>
2020-04-08 11:52:19 -0600

    Changed error text and line spacing just because :D

--- src/api/favorite-movie.php
+++ src/api/favorite-movie.php
@@ -4,35 +4,37 @@ session_start();
 include '../db-helpers.inc.php';
 // return exit(http_response_code(404));
 
-if (isset($_SESSION["u_id"])  && $_SERVER['REQUEST_METHOD'] == "POST") {
+if (isset($_SESSION["u_id"]) && $_SERVER['REQUEST_METHOD'] == "POST") {
     if (isset($_GET["movie_id"]) && isset($_GET["poster"]) && isset($_GET["title"])) {
         
         $mov_id = $_GET["movie_id"];
         $poster = $_GET["poster"];
         $title = $_GET["title"];
         $found = false;
+        
         foreach($_SESSION['fav_movies'] as $movie){
                 if($movie['id'] == $mov_id){
                     $found = true;
                 }          
         }
-        if (!$found){
+        
+        if (!$found) {
             $new_data  = array(
                 "id" => $mov_id,
                 "poster" => $poster,
                 "title" => $title 
             );
+            
             array_push($_SESSION['fav_movies'], $new_data); 
         } else {
-            echo "Already Exists";
+            echo "Already favorited"; // don't change this string. It's used to check if a movie was favorited.
         }
     } else {
-        echo "Missing parameter";
+        echo "Malformed request, missing movie_id, poster, or title";
     }
 } else {
     echo "User not logged in";
-    return json_encode(false);
+    // return json_encode(false);
+    // what is this return for? There's no function here.
 }
-
-
 ?>
