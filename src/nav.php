882deed258b209fa879872e80fb52c912d9381fb
rpowe271 <35184251+rpowe271@users.noreply.github.com>
2020-04-09 13:41:59 -0600

    Browse Movies link now appears whether signed in/out

--- src/nav.php
+++ src/nav.php
@@ -6,15 +6,16 @@
         <!-- <ul class="right hide-on-small-and-down"> -->
           <ul class="nav-list">
             <li><a href="about.php">About</a></li>
+            <li><a href='browse-movies.php'>Browse Movies</a></li>  
             <?php
             if (!isset($_SESSION['u_id'])){
-            echo  "<li><a href='login.php'>Login</a></li>";
-            echo  "<li><a href='sign-up.php'>Sign-Up</a></li>";
+                echo  "<li><a href='login.php'>Login</a></li>";
+                echo  "<li><a href='sign-up.php'>Sign-Up</a></li>";
 
             } else {
-            echo  "<li><a href='browse-movies.php'>Browse Movies</a></li>";
-            echo  "<li><a href='favorites.php'>Favorites</a></li>";
-            echo  "<li><a href='logout.php'>Logout</a></li>";
+                //echo  "<li><a href='browse-movies.php'>Browse Movies</a></li>";
+                echo  "<li><a href='favorites.php'>Favorites</a></li>";
+                echo  "<li><a href='logout.php'>Logout</a></li>";
             }
 
             ?>
