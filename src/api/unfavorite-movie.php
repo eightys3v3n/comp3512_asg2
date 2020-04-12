1f454d606c08ea79ca63456b749d5d6ed49841b7
eightys3v3n <eightys3v3n@gmail.com>
2020-04-12 14:56:59 -0600

    Finished implementing

--- src/api/unfavorite-movie.php
+++ src/api/unfavorite-movie.php
@@ -4,11 +4,12 @@ session_start();
 include '../db-helpers.inc.php';
 
 if (isset($_SESSION["u_id"]) && $_SERVER['REQUEST_METHOD'] == "GET") {
-    // echo json_encode({'a' => 1});
     $movieId = $_GET["movie_id"];
-    // for()
-    print_r($_SESSION["fav_movies"]);
-    echo $movieId;
+    
+    $_SESSION['fav_movies'] = array_filter($_SESSION['fav_movies'],
+                                           function($mov) use ($movieId) {
+            if ($mov['id'] == $movieId) { return false; }
+            else { return true; }
+        });
 }
-
 ?>
