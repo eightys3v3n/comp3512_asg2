defe80896bfede512d3405479cc5a37c7e063192
Daniel Collins <danielmcollins1996@gmail.com>
2020-04-12 21:32:06 -0700

    basic responsive nav

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", main);
 
 
-
 function main() {
     // Put stuff here. This runs after DOMContentLoaded.
-    
+    let listItems = document.querySelector('.nav-list');
+    let hamburger = document.querySelector('.navbar-toggle');
+
+    hamburger.addEventListener('click', (e) => {
+        listItems.classList.toggle("active");
+        console.log('hihi');
+    })
 }
